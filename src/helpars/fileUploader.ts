3a7fc5bb9d28c04dafcce88a69d7e43ecc2af9d5
thisIsKhalid <iamkhalid.hs@gmail.com>
2025-03-03 18:21:55 +0600

    refactor fileUploader to use dynamic storage and improved filename generation

--- src/helpars/fileUploader.ts
+++ src/helpars/fileUploader.ts
@@ -1,52 +1,53 @@
 import fs from "fs";
 import multer from "multer";
 import path from "path";
+import { v4 as uuidv4 } from "uuid";
+import { slugify } from "../utils/slugify";
+import { fileFilter } from "./fileFilter";
 
-// const uploadDir = path.join(process.cwd(), "uploads");
+const uploadDir = path.join(process.cwd(), "uploads");
 
-// // Ensure the directory exists
-// if (!fs.existsSync(uploadDir)) {
-//   fs.mkdirSync(uploadDir, { recursive: true });
-// }
+// Ensure the directory exists
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    // cb(null, path.join( "/var/www/uploads"));
-    cb(null, path.join(process.cwd(), "uploads"));
-  },
-  filename: function (req, file, cb) {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  },
-});
+// local file storage
+export const createStorage = (folder?: string) => {
+  const uploadFolder = folder
+    ? path.join(process.cwd(), "uploads", folder)
+    : path.join(process.cwd(), "uploads");
 
-// File filter for validation
-const fileFilter = (req: any, file: any, cb: any) => {
-  const allowedMimeTypes = [
-    "video/mp4",
-    "video/mov",
-    "video/avi",
-    "image/jpeg",
-    "image/png",
-    "image/jpg",
-  ];
-  if (allowedMimeTypes.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(
-      new Error(
-        `Invalid file type. Only ${allowedMimeTypes.join(", ")} are allowed`
-      ),
-      false
-    );
-  }
+  return multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, uploadFolder);
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = `${uuidv4()}-${Date.now()}`;
+      const fileExtension = path.extname(file.originalname);
+
+      const slugifiedName = slugify(
+        path.basename(file.originalname, fileExtension)
+      );
+
+      const fileName = `${slugifiedName}-${uniqueSuffix}${fileExtension}`;
+
+      cb(null, fileName);
+    },
+  });
 };
 
-export const upload = multer({ storage: storage, fileFilter: fileFilter });
+export const upload = multer({
+  storage: createStorage(),
+  fileFilter: fileFilter,
+});
 
-const uploadSingle = upload.single("carImage");
+// const example = upload.fields([{ name: "galleryImage", maxCount: 5 }]);
 
-const uploadMultiple = upload.fields([
-  { name: "singleImage", maxCount: 10 },
-  { name: "galleryImage", maxCount: 10 },
-]);
+// const anotherExample = upload.single("avatar");
 
+// const uploadRefunds = multer({ storage: createStorage("refund") });
+
+// const uploadMultiple = uploadRefunds.fields([
+//   { name: "galleryImage", maxCount: 5 },
+// ]);
