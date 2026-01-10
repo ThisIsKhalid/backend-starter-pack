7d2e08b8b159041113251c4f777f069d13c82ed4
thisIsKhalid <iamkhalid.hs@gmail.com>
2026-01-10 10:01:59 +0600

    remove old version files

--- src/app/modules/Student/student.interface.ts
+++
@@ -1,9 +0,0 @@
-export type IStudent = {
-  firstName: string;
-  lastName: string;
-  email: string;
-  phoneNumber: string;
-  role: string;
-  status: string;
-  profileImage: string;
-};
--- src/utils/slugify.ts
+++
@@ -1,8 +0,0 @@
-export const slugify = (filename: string): string => {
-  return filename
-    .toLowerCase()
-    .replace(/ /g, "-")
-    .replace(/[^\w-]+/g, "")
-    .replace(/-+/g, "-")
-    .replace(/^-+|-+$/g, "");
-};
