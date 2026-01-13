3f725aa1ad126832a18ab398286066ad4a96c640
thisIsKhalid <iamkhalid.hs@gmail.com>
2026-01-13 16:16:15 +0600

    feat: add image upload and email sending functionality
    - Updated package.json to include multer, nodemailer, and sharp for file handling and email services.
    - Enhanced configuration to support email sender credentials.
    - Implemented validateImageContent middleware to check image validity using sharp.
    - Created ApiError class for standardized error handling.
    - Developed emailSender helper for sending emails using nodemailer.
    - Added fileFilter for validating uploaded files based on MIME types.
    - Implemented fileUploadToLocal helper for handling file uploads with multer.
    - Created imageUploader for managing image uploads with specific constraints and storage options.

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,4 +9,8 @@ export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT || 8000,
   password_salt: process.env.PASSWORD_SALT || "12",
+  emailSender: {
+    email: process.env.EMAIL_SENDER_EMAIL || "",
+    app_pass: process.env.EMAIL_SENDER_APP_PASS || "",
+  }
 };
